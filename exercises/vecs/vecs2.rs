686d6e628a1b62ea2b70408f67153620ed1d5969
Dheeraj <dheeraj@appkrt.com>
2022-12-17 00:22:54 +0530

    finish exercises

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -6,13 +6,14 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// !I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *i=*i*2 //wow! this was so hard for me to discover as I've never seen rust code with a dereference operator (at least the one I understood. !). This was tiring umph!
+        //TODO learn more about dereference later on
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -23,7 +24,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|num| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        num*2
     }).collect()
 }
 
