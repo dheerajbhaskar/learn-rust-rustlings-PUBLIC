5c2bd6ae1d02d1a1a1654fcdc4fa1d327f4ccefa
Dheeraj <dheeraj@appkrt.com>
2022-12-17 21:22:44 +0530

    finish exercises

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,14 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// !I AM NOT DONE
 
 fn main() {
     let mut x = 100;
     let y = &mut x;
+    *y += 100; //dheeraj: y is dropped here because it's not referenced after this
     let z = &mut x;
-    *y += 100;
     *z += 1000;
+    // dbg!(y); //can't do this. If y referenced here, then it needs to be alive till here. That would mean two mutable references would be alive at the same time, which is not allowed.
     assert_eq!(x, 1200);
 }
--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,27 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
+// !I AM NOT DONE
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    // data = (&data).to_uppercase(); // this is not the same as `&data.to_uppercase()`. Latter is returning a reference to the String that to_uppercase() returns. I'm so glad I tried using parens around &data, otherwise I'd have never guessed & was being applied to the entire expression.
+    //& is not an address. It's better to think of & as borrow based on my understanding so far. Rust anyway corrects you if you give a reference where an actual struct is expected.
+    //wait & is overloaded I think. It functions as a borrow, and also as a reference. Maybe a borrow is a reference. Hmmm, I'm sure I can wrap my head around this in just a couple of more uses.
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
