03e9af3661434ff2ac010f7560a72c08bd672df8
Dheeraj <dheeraj@appkrt.com>
2022-12-17 14:09:23 +0530

    finish exercises

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,7 +7,8 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // let mut vec1 = fill_vec(vec0.clone());// one way. But is not satisfying, as that's not the intent of the println code
+    let mut vec1 = fill_vec(&vec0); //this and all other changes is another way. This is needs more changes but gets the println code to do what it was meant to
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,8 +18,8 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone(); // this is another way. way #2 from the hint infact
 
     vec.push(22);
     vec.push(44);
