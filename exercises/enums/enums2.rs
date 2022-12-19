52f636bd41be94e3735904cfda8950d0b99b6187
Dheeraj <dheeraj@appkrt.com>
2022-12-20 00:25:39 +0530

    finish exercises

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,16 @@
 // enums2.rs
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// !I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move{x:i32, y:i32},
+    Echo(String),
+    ChangeColor (u8, u8, u8),
+    Quit
+
 }
 
 impl Message {
@@ -15,6 +20,7 @@ impl Message {
 }
 
 fn main() {
+    //dheeraj: takeaway - enums can have any of the types of structs - unit, tuple, c-like struct, and also primitive values I think.
     let messages = [
         Message::Move { x: 10, y: 30 },
         Message::Echo(String::from("hello world")),
@@ -22,7 +28,8 @@ fn main() {
         Message::Quit,
     ];
 
-    for message in &messages {
+    //dheeraj: you can use a borrow (&messages) or an owned messages thing as well here. I think I'm starting to understand borrows and owned structs. These are almost nothing like C pointers. In fact, on the internet, these are called smart pointers because they 1. enable borrow checker to work 2. They do somethings differently I think. For example, I didn't have to do a *message to read it. Code remains the same if I do for..in messages or for..in &messages (Takeaway, & is communicating that it's a read-only borrow, not really that it's an address). *message is in fact a way IIUC to unbox the smart pointer to get at the actual value, for example to update it.
+    for message in messages {
         message.call();
     }
 }
