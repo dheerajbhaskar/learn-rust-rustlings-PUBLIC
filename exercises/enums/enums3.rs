fa8f34bcd7619fa55433c806d54db92111778e90
Dheeraj <dheeraj@appkrt.com>
2022-12-20 00:59:27 +0530

    finish exercises

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,14 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// !I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8,u8,u8)), //dheeraj: note the additional parens to show this is a tuple. The outer parens are for the enum syntax itself
+    Echo(String),
+    Move(Point),
+    Quit
 }
 
 struct Point {
@@ -38,6 +42,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message{
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(string) => self.echo(string),
+            Message::Move(point) => self.move_position(point),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
